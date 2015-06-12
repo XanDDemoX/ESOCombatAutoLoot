d5edf314f4795adff646ea4121e6f17988a6019a
XanDDemoX <XanDDemoX@users.noreply.github.com>
2015-06-12 14:02:20 +0100

    Version 0.0.2 Added configurable delay at the end of combat to allow an additional time window where auto-loot remains enabled for looting single/weaker enemies quickly. Added configuration slash command for enable/disable and restore delay

--- CombatAutoLoot/CombatAutoLoot.lua
+++ CombatAutoLoot/CombatAutoLoot.lua
@@ -1,23 +1,99 @@
 
+local _prefix = "[CombatAutoLoot]: "
+local _debug = false 
+local _settings = { enabled = true, timeout = 0 } 
 
-local _settings = { enabled = true } 
+local function trim(str)
+	if str == nil or str == "" then return str end 
+	return (str:gsub("^%s*(.-)%s*$", "%1"))
+end 
+
+local function TryParseOnOff(str)
+	local on = (str == "+" or str == "on")
+	local off = (str == "-" or str == "off")
+	if on == false and off == false then return nil end
+	return on
+end
+
+local function println(...)
+	local args = {...}
+	if #args == 0 then return end
+	for i,v in ipairs(args) do 
+		args[i] = tostring(v)
+	end 
+	table.insert(args,1,_prefix)
+	d(table.concat(args))
+end
+
+local function debugln(...)
+	if _debug == false then return end 
+	println(...)
+end
+
+local _debugTimes = {}
+local function debugtime(key,text)
+	if _debug == false then return end
+	
+	local t = _debugTimes[key] 
+	
+	if t == nil then 
+		t = GetGameTimeMilliseconds()
+		_debugTimes[key] = t
+		println(text," Start ",key," ",t)
+	else
+		_debugTimes[key] = nil
+		local tt = GetGameTimeMilliseconds()
+		println(text," Stop ", key," ",t, " : ", tt, " : ", tt - t)
+	end 
+end
 
 local function Initialise()
 
 	local _inCombat
 	local _handler
 	
+	local _origAutoLoot
+	
 	local function ReticleContainerInteractShow(...)
 		-- block in combat
-		if _inCombat == false and _handler ~= nil then 
+		if _settings.enabled == false and _handler ~= nil then
 			_handler(...)
 		end 
+		if _origAutoLoot == nil and _inCombat == false and _handler ~= nil then 
+			_handler(...)
+		end 
+	end
+	
+	local function Enable()
+		if _origAutoLoot ~= nil then return end
+		
+		
+		
+		-- get the original setting value
+		_origAutoLoot = GetSetting(SETTING_TYPE_LOOT, LOOT_SETTING_AUTO_LOOT)
+		 
+		SetSetting(SETTING_TYPE_LOOT, LOOT_SETTING_AUTO_LOOT, 1)
+		
+		debugln("Auto-Loot: Enabled")
+	end
+	
+	local DEBUG_TIME_KEY_RESTORE = "restore"
+	
+	local function Restore()
+		if _origAutoLoot == nil then return end
+		
+		
+			-- restore original
+		SetSetting(SETTING_TYPE_LOOT, LOOT_SETTING_AUTO_LOOT, _origAutoLoot)
+		_origAutoLoot = nil 
+		
+		debugtime(DEBUG_TIME_KEY_RESTORE, "Auto-Loot: Restored")
 	end
 	
 	EVENT_MANAGER:RegisterForEvent("CombatAutoLoot_CombatStateChanged",EVENT_PLAYER_COMBAT_STATE,function(eventCode, inCombat)
 		
 		
-		if _inCombat == nil then 
+		if _inCombat == nil and _settings.enabled == true then 
 			_inCombat = inCombat
 			
 			-- ensure recticle handler is ReticleContainerInteractShow
@@ -37,19 +113,57 @@ local function Initialise()
 		
 		-- enable disable auto loot based on whether in combat
 		if _inCombat == true then 
-			-- get the original setting value
-			_origAutoLoot = GetSetting(SETTING_TYPE_LOOT, LOOT_SETTING_AUTO_LOOT)
+		
+			Enable()
 			 
-			 SetSetting(SETTING_TYPE_LOOT, LOOT_SETTING_AUTO_LOOT, 1)
-			 
-		elseif _origAutoLoot ~= nil then 
-			-- restore original
-			SetSetting(SETTING_TYPE_LOOT, LOOT_SETTING_AUTO_LOOT, _origAutoLoot)
-			_origAutoLoot = nil 
-		end
+		elseif _origAutoLoot ~= nil then
 			
+			debugtime(DEBUG_TIME_KEY_RESTORE, "Combat Ended ")
+			
+			local timeout = _settings.timeout 
+
+			if timeout > 0 then 
+				zo_callLater(function() if _inCombat == false then Restore() end end ,timeout)
+			else
+				
+				Restore()
+			end
+			
+		end
 		
 	end)
+	
+	local _maxTimeout = 10000
+	
+	SLASH_COMMANDS["/cal"] = function(args)
+		args = trim(args)
+		
+		if args == nil or args == "" then return end 
+		
+		local timeout = tonumber(args)
+		
+		if timeout ~= nil then
+		
+			if timeout >= 0 and timeout <= _maxTimeout then
+				_settings.timeout = timeout
+				println("Restore delay ",timeout,"ms")
+			else
+				println("Restore delay ",timeout," out of range (0-",_maxTimeout,"ms)")
+			end
+			
+		else
+			
+			local onoff = TryParseOnOff(args)
+			
+			if onoff == nil then return end 
+			
+			_settings.enabled = onoff
+			
+			println((_settings.enabled and "Enabled") or "Disabled")
+			
+		end
+	
+	end
 
 end 
 
--- README.md
+++ README.md
@@ -1,12 +1,27 @@
-# ESOCombatAutoLoot v0.0.1
+# ESOCombatAutoLoot v0.0.2
 A simple addon for The Elder Scrolls Online which enables Auto-Loot during combat.
 The value of the Auto-Loot setting at the time of entering combat is restored upon leaving combat. (It won't turn off Auto-Loot if it is already enabled).
 
-[Download the latest version](https://raw.githubusercontent.com/XanDDemoX/ESOCombatAutoLoot/master/zips/Combat%20Auto%20Loot%200.0.1.zip)
+[Download the latest version](https://raw.githubusercontent.com/XanDDemoX/ESOCombatAutoLoot/master/zips/Combat%20Auto%20Loot%200.0.2.zip)
+
+Slash Commands 
+=============
+
+* /cal on - Enables Combat Auto loot 
+* /cal +
+
+* /cal off - Disables Combat Auto Loot
+* /cal - 
+
+* /cal delayMs - Sets the delay between the end of combat and reset of the Auto-Loot setting in milliseconds. Range: 0-10000ms (0-10 seconds)
+* /cal 4000 - Sets the delay to 4 seconds. 
 
 
 Change Log
 =============
+* **Version 0.0.2**
+  * Added configurable delay at the end of combat to allow an additional time window where auto-loot remains enabled for looting single/weaker enemies quickly.
+  * Added configuration slash command for enable/disable and restore delay
 * **Version 0.0.1**
   * Initial Release
 
